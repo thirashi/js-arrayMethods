b489c41864ebf899f350e8bb575a3bc1940fe21c
Tyler Hirashima <tylerhirashima@Tylers-MacBook-Pro-5.local>
2018-02-07 19:44:47 -1000

    Completed Ex4

--- exercise.js
+++ exercise.js
@@ -45,13 +45,16 @@ var yoda = ["try", "no", "is", "there", "not", "do", "or", "do"];
  console.log(yoda);
 
 
-
 /*The array.shift() removes the first element from an array and returns that element*/
 
 /*4. People been lining up for hours to get the newest iphone release. Help manage the unruly crowd of privileged customers by serving them one at a time and assiging it to a variable named `nowServing`. Console.log this new variable as well as the waitList.*/
 
 var waitList = ["Chance the Rapper", "Khalid", "Tay-Tay", "Barry Manilow", "Piko Taro"];
+console.log(waitList);
 
+var nowServing = waitList.shift();
+console.log(nowServing);
+console.log(waitList);
 
 
 /*The array.unshift() adds one or more elements to the front of an array and returns the new length of the array*/
